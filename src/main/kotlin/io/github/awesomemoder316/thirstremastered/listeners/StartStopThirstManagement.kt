1d6cf99916e4c5add83196e0ad461926fcadb3df
awesomemoder <thesusefulthing@gmail.com>
2021-09-06 20:08:57 -0700

    Fix bug where player joining in creative or spectator would still be added to PlayerDataManager.

--- src/main/kotlin/io/github/awesomemoder316/thirstremastered/listeners/StartStopThirstManagement.kt
+++ src/main/kotlin/io/github/awesomemoder316/thirstremastered/listeners/StartStopThirstManagement.kt
@@ -4,22 +4,14 @@ package io.github.awesomemoder316.thirstremastered.listeners
 import dev.jcsoftware.jscoreboards.JPerPlayerMethodBasedScoreboard
 import dev.jcsoftware.jscoreboards.JPerPlayerScoreboard
 import io.github.awesomemoder316.thirstremastered.data.IPlayerDataManager
-import io.github.awesomemoder316.thirstremastered.data.ViewTypes
-import net.kyori.adventure.bossbar.BossBar
-import net.kyori.adventure.platform.bukkit.BukkitAudiences
-import net.kyori.adventure.text.Component
-import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerGameModeChangeEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
-import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.scheduler.BukkitRunnable
-import java.util.*
 
-class StartStopPassiveThirst(private val iPlayerDataManager: IPlayerDataManager): Listener {
+class StartStopThirstManagement(private val iPlayerDataManager: IPlayerDataManager): Listener {
 
     //Remove player from list when in creative/spectator, as they should be immune from thirst.
 
@@ -44,7 +36,10 @@ class StartStopPassiveThirst(private val iPlayerDataManager: IPlayerDataManager)
     @EventHandler
     fun onJoin(e: PlayerJoinEvent) {
         if (e.player.hasPermission("thirstremastered.nothirst")) return
-        iPlayerDataManager.addPlayer(e.player.uniqueId)
+        when (e.player.gameMode) {
+            GameMode.ADVENTURE, GameMode.SURVIVAL -> iPlayerDataManager.addPlayer(e.player.uniqueId)
+            else -> return
+        }
     }
 
     @EventHandler
